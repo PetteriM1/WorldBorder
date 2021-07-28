10cdd46857f9b2339a9131c78df8baaa7b73a086
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-07-29 01:46:41 +0300

    Update

--- src/main/java/me/petterim1/worldborder/Main.java
+++ src/main/java/me/petterim1/worldborder/Main.java
@@ -16,72 +16,93 @@ import java.util.List;
 
 public class Main extends PluginBase implements Listener {
     
-    private int distance;
+    private long distance;
     private String message;
     private String messageTp;
+    private String messageTpOp;
     private boolean teleportToSpawn;
     private boolean checkVehicleMovement;
+    private boolean opBypass;
     private List<String> worlds;
+    private static final Vector3 temp = new Vector3(0, 0, 0);
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
-        if (getConfig().getInt("configVersion") != 3) {
-            if (getConfig().getInt("configVersion") == 2) {
+        int ver = 4;
+        int cfg = getConfig().getInt("configVersion", 99);
+        if (cfg != ver) {
+            if (cfg == 3) {
                 Config c = getConfig();
-                c.set("configVersion", 3);
-                c.set("messageTp", "§cYou cannot teleport outside world border!");
+                c.set("configVersion", ver);
+                c.set("messageTpOp", "§6You have passed the world border");
+                c.set("opBypass", false);
+                c.save();
+                getLogger().info("Config updated from version " + cfg);
+            } else if (cfg < 3) {
+                Config c = getConfig();
+                c.set("configVersion", ver);
+                c.set("messageTp", "§cYou cannot teleport outside the world border!");
+                c.set("messageTpOp", "§6You have passed the world border");
                 c.set("teleportToSpawn", false);
                 c.set("checkVehicleMovement", false);
+                c.set("opBypass", false);
                 c.set("worlds", Arrays.asList("world"));
                 c.save();
+                getLogger().info("Config updated from version " + cfg);
             } else {
-                Config c = getConfig();
-                c.set("configVersion", 3);
-                c.set("checkVehicleMovement", false);
-                c.set("worlds", Arrays.asList("world"));
-                c.save();
+                getLogger().warning("Couldn't update the config file: unknown config version");
             }
         }
-        distance = (int) Math.pow(getConfig().getInt("distance"), 2);
+        distance = (long) Math.pow(getConfig().getInt("distance"), 2);
         message = getConfig().getString("message");
         messageTp = getConfig().getString("messageTp");
+        messageTpOp = getConfig().getString("messageTpOp");
         teleportToSpawn = getConfig().getBoolean("teleportToSpawn");
         checkVehicleMovement = getConfig().getBoolean("checkVehicleMovement");
+        opBypass = getConfig().getBoolean("opBypass");
         worlds = getConfig().getStringList("worlds");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
-        if (!worlds.contains(p.getLevel().getName())) return;
-        Position spawn = p.getLevel().getSpawnLocation();
-        if (p.distanceSquared(new Vector3(spawn.getX(), p.getY(), spawn.getZ())) > distance) {
-            p.sendMessage(message);
-            e.setCancelled(true);
-            if (teleportToSpawn) {
-                p.teleport(p.getSpawn(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+        if (worlds.contains(p.getLevel().getName())) {
+            Position spawn = p.getLevel().getSpawnLocation();
+            if (p.distanceSquared(temp.setComponents(spawn.getX(), p.getY(), spawn.getZ())) > distance) {
+                if (!(opBypass && p.isOp())) {
+                    p.sendMessage(message);
+                    e.setCancelled(true);
+                    if (teleportToSpawn) {
+                        p.teleport(p.getSpawn(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+                    }
+                }
             }
         }
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onTeleport(PlayerTeleportEvent e) {
-        if (!worlds.contains(e.getTo().getLevel().getName())) return;
-        Position spawn = e.getTo().getLevel().getSpawnLocation();
-        if (e.getTo().distanceSquared(new Vector3(spawn.getX(), e.getTo().getY(), spawn.getZ())) > distance) {
-            e.getPlayer().sendMessage(messageTp);
-            e.setCancelled(true);
+        Player p = e.getPlayer();
+        if (worlds.contains(e.getTo().getLevel().getName())) {
+            Position spawn = e.getTo().getLevel().getSpawnLocation();
+            if (e.getTo().distanceSquared(temp.setComponents(spawn.getX(), e.getTo().getY(), spawn.getZ())) > distance) {
+                if (!(opBypass && p.isOp())) {
+                    p.sendMessage(messageTp);
+                    e.setCancelled(true);
+                } else {
+                    p.sendMessage(messageTpOp);
+                }
+            }
         }
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onVehicleMove(VehicleMoveEvent e) {
-        if (!worlds.contains(e.getTo().getLevel().getName())) return;
-        if (checkVehicleMovement) {
+        if (checkVehicleMovement && worlds.contains(e.getTo().getLevel().getName())) {
             Position spawn = e.getTo().getLevel().getSpawnLocation();
-            if (e.getTo().distanceSquared(new Vector3(spawn.getX(), e.getTo().getY(), spawn.getZ())) > distance) {
+            if (e.getTo().distanceSquared(temp.setComponents(spawn.getX(), e.getTo().getY(), spawn.getZ())) > distance) {
                 e.getVehicle().kill();
             }
         }
